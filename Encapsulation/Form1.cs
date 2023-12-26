92fd9c8d9d060c601db52b2b22bc5b29e28d2ef7
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2023-12-26 22:36:24 +0300

    Encapsulation projesi eklendi.

---
+++ Encapsulation/Form1.cs
@@ -0,0 +1,30 @@
+﻿using Encapsulation.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encapsulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Account account = new Account();
+            account.userName = txtUserName.Text;
+            account.password = txtPassword.Text;
+            MessageBox.Show($"Kullanıcı başarıyla oluşturuldu: {account.userName}");
+
+        }
+    }
+}
---
+++ Encapsulation/Models/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encapsulation.Models
+{
+    public class Account
+    {
+        public Account() // Consructor
+        {
+            int a = 5; //local veriable: Bu değişken sadece burada kullanılır
+
+        }
+        int b = 5; // field (Global değişken)
+
+        string _userName;
+
+        public string userName
+        {
+            get { return _userName; } //sürekli kendini dönememesi için farklı bir değişken atadık
+            set
+            {
+                if (value.Length > 3)
+                {
+                    _userName = value;
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Lütfen 3 karakterden fazla giriniz.");
+                    Application.Restart();
+                }
+            } 
+        }//Encapsulation: Kapsülleme işlemi.
+        public string password { get; set; }
+    }
+}
