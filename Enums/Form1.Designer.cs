68b032cb752495ae31a2b46f54dfaef28daf00b3
Ahmet Selim KISA <ahmet---ultraaslan@hotmail.com>
2024-01-02 23:41:42 +0300

    Enums dersi eklendi.

---
+++ Enums/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Enums
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.cmbDiff = new System.Windows.Forms.ComboBox();
+            this.radioMan = new System.Windows.Forms.RadioButton();
+            this.radioWoman = new System.Windows.Forms.RadioButton();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(269, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New Game";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(139, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User Name: ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(178, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Difficulty:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUserName.Location = new System.Drawing.Point(311, 182);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(180, 38);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // cmbDiff
+            // 
+            this.cmbDiff.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbDiff.FormattingEnabled = true;
+            this.cmbDiff.Location = new System.Drawing.Point(311, 297);
+            this.cmbDiff.Name = "cmbDiff";
+            this.cmbDiff.Size = new System.Drawing.Size(180, 39);
+            this.cmbDiff.TabIndex = 4;
+            // 
+            // radioMan
+            // 
+            this.radioMan.AutoSize = true;
+            this.radioMan.Location = new System.Drawing.Point(311, 249);
+            this.radioMan.Name = "radioMan";
+            this.radioMan.Size = new System.Drawing.Size(53, 17);
+            this.radioMan.TabIndex = 5;
+            this.radioMan.TabStop = true;
+            this.radioMan.Text = "Erkek";
+            this.radioMan.UseVisualStyleBackColor = true;
+            // 
+            // radioWoman
+            // 
+            this.radioWoman.AutoSize = true;
+            this.radioWoman.Location = new System.Drawing.Point(420, 249);
+            this.radioWoman.Name = "radioWoman";
+            this.radioWoman.Size = new System.Drawing.Size(52, 17);
+            this.radioWoman.TabIndex = 6;
+            this.radioWoman.TabStop = true;
+            this.radioWoman.Text = "Kadın";
+            this.radioWoman.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(368, 370);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(123, 46);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.radioWoman);
+            this.Controls.Add(this.radioMan);
+            this.Controls.Add(this.cmbDiff);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.ComboBox cmbDiff;
+        private System.Windows.Forms.RadioButton radioMan;
+        private System.Windows.Forms.RadioButton radioWoman;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Enums/Form1.cs
@@ -0,0 +1,44 @@
+﻿using Enums.Enums;
+using Enums.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enums
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Game game = new Game();
+            game.userName = txtUserName.Text;
+            game.difficulty = (Difficulty)cmbDiff.SelectedItem;
+            game.gender = radioMan.Checked ? Gender.Male : Gender.Female;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbDiff.Items.Add(Difficulty.Easy);
+            cmbDiff.Items.Add(Difficulty.Medium); 
+            cmbDiff.Items.Add(Difficulty.Hard);
+
+
+        }
+    }
+}
